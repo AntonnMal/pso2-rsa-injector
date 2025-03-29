7f1c3d94c1293604a22362a329b4489cb68f5926
Anton Maltsev <nokiaru9999@gmail.com>
2025-03-29 23:38:47 +0700

    Fix latest nightly compilation errors
    dll-syringe uses now deleted methods, so as an interim solution we use
    our own fork of the library

--- src/lib.rs
+++ src/lib.rs
@@ -248,7 +248,7 @@ extern "system" fn crypt_open_stub(
         let orig_key = unsafe { slice::from_raw_parts(pbdata, dwdatalen as usize) };
         {
             let mut keys = SEGA_RSA_KEYS.write();
-            if keys.len() == 0 {
+            if keys.is_empty() {
                 if let Some(x) = get_rsa_key().unwrap_window() {
                     *keys = x;
                 }
